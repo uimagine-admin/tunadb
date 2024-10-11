5985c83cc785a738e810363281f4a6a39adf3e4f
Vincentius Roger Kuswara <vincentiusrkuswara@gmail.com>
2024-10-11 22:46:12 +0800

    fix: correct node.go struct

--- internal/types/node.go
+++ internal/types/node.go
@@ -4,12 +4,12 @@ import "fmt"
 
 type Node struct {
 	IPAddress string
-	id        string
-	Port      string
+	ID        string
+	Port      uint64
 	Name      string
 }
 
 // for printing purposes and debug
 func (n *Node) String() string {
-	return fmt.Sprintf("Node(ID = %s) with (PORT= %s) with IP = %s, with Name = %s", n.id, n.Port, n.IPAddress, n.Name)
+	return fmt.Sprintf("Node(ID = %s) with (PORT= %d) with IP = %s, with Name = %s", n.ID, n.Port, n.IPAddress, n.Name)
 }
