2def40a65bc1c7fc95da75cd1a152a748f57b55b
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-11-06 21:06:10 +0800

    for CheckTableExists

--- internal/db/utils_test.go
+++ internal/db/utils_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestReadJSON(t *testing.T) {
 	// Create a temporary JSON file for testing
-	filename := "../data/sample.json"
+	filename := "../data/sample.json" // change this to whatever suits u
 
 	// Test ReadJSON function
 	data, err := ReadJSON(filename)
@@ -17,5 +17,20 @@ func TestReadJSON(t *testing.T) {
 		t.Errorf("ReadJSON returned unexpected data: %s", data)
 	} else {
 		t.Logf("ReadJSON succeeded: %s", data)
+	}
+}
+
+func TestCheckTableExists(t *testing.T) {
+	filename := "../data/sample.json" // change this to whatever suits u
+
+	data, err := ReadJSON(filename)
+
+	t.Logf("ReadJSON retrieved: %s, %s", data, err)
+
+	if !CheckTableExists("sample", data) {
+		t.Errorf("CheckTableExists failed to find existing table")
+	}
+	if CheckTableExists("other-name", data) {
+		t.Errorf("CheckTableExists found non-existent table")
 	}
 }
