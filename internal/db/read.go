610d8d04d437ab659414c33dcce40f4eadf2f910
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-11-07 23:10:02 +0800

    read done

--- internal/db/read.go
+++ internal/db/read.go
@@ -1,10 +1,48 @@
 package db
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	pb "github.com/uimagine-admin/tunadb/api"
 )
 
-func HandleRead(nodeId int, req pb.ReadRequest) {
-	// return a list of (all object), with `pageID == pageID` in `nodeID.json`
-	// return list, error
+type Row struct {
+	PageId    string `json:"pageID"`
+	Element   string `json:"element"`
+	Timestamp string `json:"timestamp"`
+	Event     string `json:"event"`
+	UpdatedAt string `json:"updated_at"`
+	CreatedAt string `json:"created_at"`
+}
+
+func HandleRead(nodeId int, req *pb.ReadRequest) ([]Row, error) {
+	filename := fmt.Sprintf("../data/%d.json", nodeId)
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	byteValue, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	var rows []Row
+	err = json.Unmarshal(byteValue, &rows)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+
+	var result []Row
+	for _, event := range rows {
+		if event.PageId == req.PageId {
+			result = append(result, event)
+		}
+	}
+
+	return result, nil
 }
--- internal/db/read_test.go
+++ internal/db/read_test.go
@@ -1,7 +1,26 @@
 package db
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	pb "github.com/uimagine-admin/tunadb/api"
+)
 
 func TestHandleRead(t *testing.T) {
+	nodeId := 0
+	fmt.Println("test ", nodeId)
 
+	req := &pb.ReadRequest{
+		PageId: "page1",
+	}
+
+	rows, err := HandleRead(nodeId, req)
+	if err != nil {
+		t.Fatalf("HandleRead failed: %s", err)
+	}
+
+	for _, row := range rows {
+		fmt.Printf("Event: %+v\n", row)
+	}
 }
